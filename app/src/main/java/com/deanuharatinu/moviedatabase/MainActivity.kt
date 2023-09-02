2c35f48552ad828b67f6e92b6a4356bbb1ef4dab
deanuharatinu <deanu.alt@gmail.com>
2023-09-02 16:28:02 +0700

    navigation bottom color and layout

--- app/src/main/java/com/deanuharatinu/moviedatabase/MainActivity.kt
+++ app/src/main/java/com/deanuharatinu/moviedatabase/MainActivity.kt
@@ -3,8 +3,6 @@ package com.deanuharatinu.moviedatabase
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.deanuharatinu.moviedatabase.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,17 +20,9 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun initNavBottom() {
-    setSupportActionBar(binding.toolbar)
-
     val navHostFragment =
       supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
     val navController = navHostFragment.navController
     binding.navView.setupWithNavController(navController)
-    val appBarConfiguration = AppBarConfiguration(
-      setOf(
-        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-      )
-    )
-    setupActionBarWithNavController(navController, appBarConfiguration)
   }
 }
